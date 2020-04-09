a3be830171b945f7e6090d1199700cface7bff44
Amal Jose A <howareyouami@gmail.com>
2020-04-09 12:34:53 +0530

    Add getValidNeibhours

--- src/components/algorithm.js
+++ src/components/algorithm.js
@@ -1,38 +1,49 @@
-const getNeibhours = (coordinate) => {
-    return ([
-        [coordinate[0] - 1, coordinate[1]],
-        [coordinate[0] - 1, coordinate[1] + 1],
-        [coordinate[0], coordinate[1] + 1],
-        [coordinate[0] + 1, coordinate[1] + 1],
-        [coordinate[0] + 1, coordinate[1]],
-        [coordinate[0] + 1, coordinate[1] - 1],
-        [coordinate[0], coordinate[1] - 1],
-        [coordinate[0] - 1, coordinate[1] - 1]
-    ])
-}
-let visited = []
-
+let visitedArray = []
+let neibhoursValidArray = []
 const delay = ms => new Promise(res => setTimeout(res, ms));
 
-export const floodFillAlgo = async (image, newColor, oldColor, coordinate, setImage) => {
-    if (visited.includes(JSON.stringify(coordinate))) return
-    if (
-        coordinate[0] < image.length &&
-        coordinate[0] >= 0 &&
-        coordinate[1] < image.length &&
-        coordinate[1] >= 0 &&
-        image[coordinate[0]][coordinate[1]] === oldColor
-    ) {
-        image[coordinate[0]][coordinate[1]] = newColor
-        visited.push(JSON.stringify([coordinate[0], coordinate[1]]))
-        setImage(JSON.parse(JSON.stringify(image)))
-        await delay(50)
-    } else return
-    const neibhours = getNeibhours(coordinate)
 
-    for (const neighCoord of neibhours) {
+const getValidNeibhours = (currentCoordinate, image, oldColor) => {
+    const allNeibhours = [
+        [currentCoordinate[0] - 1, currentCoordinate[1]],
+        [currentCoordinate[0] - 1, currentCoordinate[1] + 1],
+        [currentCoordinate[0], currentCoordinate[1] + 1],
+        [currentCoordinate[0] + 1, currentCoordinate[1] + 1],
+        [currentCoordinate[0] + 1, currentCoordinate[1]],
+        [currentCoordinate[0] + 1, currentCoordinate[1] - 1],
+        [currentCoordinate[0], currentCoordinate[1] - 1],
+        [currentCoordinate[0] - 1, currentCoordinate[1] - 1]
+    ]
+    const validNeighbours = allNeibhours.filter((neibourCoordinate) => {
+        if (
+            neibourCoordinate[0] < image.length &&
+            neibourCoordinate[0] >= 0 &&
+            neibourCoordinate[1] < image.length &&
+            neibourCoordinate[1] >= 0 &&
+            image[neibourCoordinate[0]][neibourCoordinate[1]] === oldColor &&
+            !visitedArray.includes(JSON.stringify(neibourCoordinate)) &&
+            !neibhoursValidArray.includes(JSON.stringify(neibourCoordinate))
+        ) {
+            debugger
+            neibhoursValidArray.push(JSON.stringify(neibourCoordinate))
+            return true
+        }
+    })
+    return validNeighbours
+}
+
+export const floodFillAlgo = async (image, newColor, oldColor, coordinate, setImage) => {
+
+    image[coordinate[0]][coordinate[1]] = newColor
+    visitedArray.push(JSON.stringify([coordinate[0], coordinate[1]]))
+    setImage(JSON.parse(JSON.stringify(image)))
+    await delay(50)
+
+    const newNeibhours = getValidNeibhours(coordinate, image, oldColor)
+    for (const neighCoord of newNeibhours) {
         await delay(50)
         floodFillAlgo(image, newColor, oldColor, neighCoord, setImage)
     }
-    return true
+    console.log("neibhoursValidArray.length", neibhoursValidArray, "visitedArray.length", visitedArray)
+    if ((neibhoursValidArray.length + 1) === visitedArray.length) return true
 }
