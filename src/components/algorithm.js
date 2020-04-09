1d19377155ff7c26bb36f79b303d576e0308f4ef
Amal Jose A <howareyouami@gmail.com>
2020-04-09 12:57:51 +0530

    Remove unwanted neibhours

--- src/components/algorithm.js
+++ src/components/algorithm.js
@@ -6,13 +6,9 @@ const delay = ms => new Promise(res => setTimeout(res, ms));
 const getValidNeibhours = (currentCoordinate, image, oldColor) => {
     const allNeibhours = [
         [currentCoordinate[0] - 1, currentCoordinate[1]],
-        [currentCoordinate[0] - 1, currentCoordinate[1] + 1],
         [currentCoordinate[0], currentCoordinate[1] + 1],
-        [currentCoordinate[0] + 1, currentCoordinate[1] + 1],
         [currentCoordinate[0] + 1, currentCoordinate[1]],
-        [currentCoordinate[0] + 1, currentCoordinate[1] - 1],
         [currentCoordinate[0], currentCoordinate[1] - 1],
-        [currentCoordinate[0] - 1, currentCoordinate[1] - 1]
     ]
     const validNeighbours = allNeibhours.filter((neibourCoordinate) => {
         if (
@@ -37,11 +33,11 @@ export const floodFillAlgo = async (image, newColor, oldColor, coordinate, setIm
     image[coordinate[0]][coordinate[1]] = newColor
     visitedArray.push(JSON.stringify([coordinate[0], coordinate[1]]))
     setImage(JSON.parse(JSON.stringify(image)))
-    await delay(50)
+    await delay(100)
 
     const newNeibhours = getValidNeibhours(coordinate, image, oldColor)
     for (const neighCoord of newNeibhours) {
-        await delay(50)
+        await delay(100)
         floodFillAlgo(image, newColor, oldColor, neighCoord, setImage)
     }
     console.log("neibhoursValidArray.length", neibhoursValidArray, "visitedArray.length", visitedArray)
