ed8e601e2281496ec666ad9a87d16139a0ff7653
Amal Jose A <howareyouami@gmail.com>
2020-04-10 20:20:33 +0530

    Add menu

--- src/components/FloodFill/index.js
+++ src/components/FloodFill/index.js
@@ -22,8 +22,8 @@ const FloodFill = () => {
     }
 
     useEffect(() => {
-        changeDimensions(30)
-    })
+        changeDimensions(10)
+    }, [])
 
     const onCellClick = async (a, b) => {
         if (flooding) return
@@ -41,15 +41,31 @@ const FloodFill = () => {
                 </h1>
                 <div>
                     <table>
-                        {image.map((row, a) => <tr>{row.map((cellColor, b) => <td style={{ backgroundColor: cellColor }} 
-                        onClick={() => {
-                            onCellClick(a, b)
-                        }}></td>)}</tr>)}
+                        {image.map((row, a) => <tr>{row.map((cellColor, b) => <td style={{ backgroundColor: cellColor }}
+                            onClick={() => {
+                                onCellClick(a, b)
+                            }}></td>)}</tr>)}
                     </table>
                 </div>
                 {flooding && <p>Flooding...</p>}
             </Container>
-            <Menu></Menu>
+            {!flooding && <Menu>
+                <div>
+                    <p>Dimensions {image.length} * {image.length}</p>
+                </div>
+                <div>
+                    <select
+                        selected={10}
+                        onChange={(e) => {
+                            changeDimensions(Number(e.target.value))
+                        }} >
+                        <option value={10}>10</option>
+                        <option value={20}>20</option>
+                        <option value={30}>30</option>
+                        <option value={40}>40</option>
+                    </select>
+                </div>
+            </Menu>}
         </>
     )
 }
--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,7 +1,18 @@
 import React from 'react'
-
+import './Menu.css'
 const Menu = ({ children }) => {
-    return <div style={{ height: 100, width: "100vw", border: "3px solid black", position: "absolute", bottom: 0 }}>
+    return <div
+        className="menu-container"
+        style={{
+            height: 100,
+            width: "100vw",
+            border: "3px solid black",
+            position: "absolute",
+            bottom: 0,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+        }}>
         {children}
     </div>
 }
