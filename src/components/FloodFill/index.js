0418ae15e2976718ca09794bdd5ed049d8e6bfde
Amal Jose A <howareyouami@gmail.com>
2020-04-11 11:07:15 +0530

    Lot of Style fixes

--- src/components/FloodFill/index.js
+++ src/components/FloodFill/index.js
@@ -63,7 +63,6 @@ const FloodFill = () => {
                         <option value={"10"}>10</option>
                         <option value={"20"}>20</option>
                         <option value={"30"}>30</option>
-                        <option value={"40"}>40</option>
                     </select>
                 </div>
                 <div>
