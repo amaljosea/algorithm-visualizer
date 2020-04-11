4dd4f60823576060dcae7c5d00399dad5d8c10a3
Amal Jose A <howareyouami@gmail.com>
2020-04-11 11:13:44 +0530

    Change style

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -4,14 +4,13 @@ const Menu = ({ children }) => {
     return <div
         className="menu-container"
         style={{
-            height: 100,
             width: "100vw",
-            border: "3px solid black",
             position: "absolute",
             bottom: 0,
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
+            backgroundColor: "silver"
         }}>
         {children}
     </div>
