5418f2a427ce558abdc385e8abe914a400fe757e
Amal Jose A <howareyouami@gmail.com>
2020-04-13 13:51:54 +0530

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
 import FloodFill from './pages/FloodFill/index'
-
+import GraphTravesal from './pages/GraphTravesal/index'
+import { Router, Switch, Route } from 'react-router'
+import { BrowserRouter } from 'react-router-dom'
 function App() {
   return (
-    <div>
-      <FloodFill />
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path='/' exact component={FloodFill} />
+        <Route path='/graph' exact component={GraphTravesal} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
