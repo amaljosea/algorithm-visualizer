f15993e8cb17e5cb69d399c58023e07ea57a1569
Amal <howareyouami@gmail.com>
2021-04-07 08:39:33 +0530

    Hirearchy code added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import './App.css';
-import FloodFill from './pages/FloodFill/index'
+import React from "react";
+import "./App.css";
+import FloodFill from "./pages/FloodFill/index";
+import HirearchyVirtualization from "./pages/HirearchyVirtualization/index";
 
 function App() {
   return (
     <div>
-      <FloodFill />
+      {/* <FloodFill /> */}
+      <HirearchyVirtualization />
     </div>
   );
 }
---
+++ src/pages/HirearchyVirtualization/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./index.css";
+
+export default function HirearchyVirtualization() {
+  return (
+    <div className="container">
+      <div className="playground">
+        <h1 className="heading">Playground</h1>
+      </div>
+      <div className="control">
+        <h1 className="heading">Controls</h1>
+      </div>
+    </div>
+  );
+}
